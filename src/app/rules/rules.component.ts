4a8c537d7ae6010fde9eb245bfbb188b052746fe
capottier <camille.pottier2@soprasteria.com>
2021-04-30 16:13:29 +0200

    fix json file

--- src/app/rules/rules.component.ts
+++ src/app/rules/rules.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService, DataType } from '../data-model/data/data.service';
 
 @Component({
   selector: 'pr-rules',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RulesComponent implements OnInit {
 
-  constructor() {
+  constructor(private dataService: DataService) {
+    this.dataService.tournamentRulesEmitter.subscribe(result => {
+      console.log(result);
+    });
+    this.dataService.askData(DataType.Rules);
   }
 
   ngOnInit(): void {
